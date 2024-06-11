bf574bd824ba5792b9c2fc9fb36aee1b8e2bae5c
PIGNATELLI\a3446 <samuel.miralles@inycom.es>
2024-06-11 13:09:31 +0200

    Añadir row_number para quedarnos con los ficheros más actuales

--- PROCEDURES/EXT.SP_CARGAR_POLIZAS_TRASPASO.sql
+++ PROCEDURES/EXT.SP_CARGAR_POLIZAS_TRASPASO.sql
@@ -26,39 +26,49 @@ BEGIN
 	CALL EXT.LIB_GLOBAL_CESCE:w_debug(i_Tenant, 'INICIO PROCEDIMIENTO with SESSION_USER ' || SESSION_USER || ' version ' || cVersion, vProcedure, io_contador);
 	
 
-	-- detectar aquellas polizas que no están incluidas
+	-- detectar aquellas polizas que no están incluidas. Añadimos row_number para quedarnos con los ficheros más actuales
 	INSERT INTO TCMP.CS_STAGEGENERICCLASSIFIER(TENANTID, BATCHNAME, CLASSIFIERID, GENERICCLASSIFIERTYPENAME, EFFECTIVESTARTDATE, EFFECTIVEENDDATE, CATEGORYTREENAME, CATEGORYNAME, BUSINESSUNITNAME, GENERICCLASSIFIERNAME, DESCRIPTION)
-	SELECT DISTINCT 
-		:i_Tenant AS TENANTID,
-		:batchname AS BATCHNAME,
-		LPAD( mvc.NUM_POLIZA, 8, '0' ) AS CLASSIFIERID,
-		'Forma de Pago' AS GENERICCLASSIFIERTYPENAME,
-		'2022-01-01' AS EFFECTIVESTARTDATE,
-		'2200-01-01' AS EFFECTIVEENDDATE,
-		'Polizas Traspaso Tree' AS CATEGORYTREENAME,
-		'Polizas Traspaso' AS CATEGORYNAME,
-		CASE
-			WHEN mvc.IDPAIS = '116' THEN 'Spain'
-			WHEN mvc.IDPAIS = '151' THEN 'Portugal'
+	SELECT :i_Tenant AS TENANTID
+		, :batchname AS BATCHNAME
+		, LPAD( NUM_POLIZA, 8, '0' ) AS CLASSIFIERID
+		, 'Forma de Pago' AS GENERICCLASSIFIERTYPENAME
+		, '2022-01-01' AS EFFECTIVESTARTDATE
+		, '2200-01-01' AS EFFECTIVEENDDATE
+		, 'Polizas Traspaso Tree' AS CATEGORYTREENAME
+		, 'Polizas Traspaso' AS CATEGORYNAME
+		, CASE
+			WHEN IDPAIS = '116' THEN 'Spain'
+			WHEN IDPAIS = '151' THEN 'Portugal'
 			ELSE ''
-		END AS BUSINESSUNITNAME,
-		LPAD( mvc.IDMODALIDAD, 3, '0' ) AS GENERICCLASSIFIERNAME,
-		mvc.BATCHNAME AS BATCHNAME_ORIGEN
-	FROM EXT.EXT_MOVIMIENTO_CARTERA_CREDITO_HIST AS mvc
-	WHERE TRIM(FECHA_TRASPASO_POL_O) <> ''
-		AND (DESC_SIT_POL_O IN ('FINALIZADO', 'CONFIRMADO'))
-		AND ((LPAD(IDMODALIDAD, 3, '0'), LPAD(NUM_POLIZA, 8, '0')) NOT IN (
-			SELECT DISTINCT 
-				clas.NAME AS NUEVA_MODALIDAD,
-				clas.CLASSIFIERID AS NUEVA_POLIZA
-			FROM 
-			cs_classifier AS clas,
-			cs_genericclassifier AS cgc,
-			cs_genericclassifiertype AS cgct
-			WHERE clas.classifierseq = cgc.classifierseq
-				AND clas.selectorid = cgct.genericclassifiertypeseq
-				AND cgct.NAME = 'Forma de Pago')
-		);
+		END AS BUSINESSUNITNAME
+		, LPAD( IDMODALIDAD, 3, '0' ) AS GENERICCLASSIFIERNAME
+		, BATCHNAME_ORIGEN
+	FROM (
+		SELECT DISTINCT 		
+			mvc.NUM_POLIZA
+			, mvc.IDPAIS
+			, mvc.IDMODALIDAD
+			, mvc.BATCHNAME AS BATCHNAME_ORIGEN
+			, ROW_NUMBER() OVER (PARTITION BY mvc.num_poliza ORDER BY mvc.createdate DESC) AS rn
+		FROM EXT.EXT_MOVIMIENTO_CARTERA_CREDITO_HIST AS mvc
+		WHERE TRIM(FECHA_TRASPASO_POL_O) <> ''
+			AND (DESC_SIT_POL_O IN ('FINALIZADO', 'CONFIRMADO'))
+			AND ((LPAD(IDMODALIDAD, 3, '0'), LPAD(NUM_POLIZA, 8, '0')) NOT IN (
+				SELECT DISTINCT 
+					clas.NAME AS NUEVA_MODALIDAD,
+					clas.CLASSIFIERID AS NUEVA_POLIZA
+				FROM 
+				cs_classifier AS clas,
+				cs_genericclassifier AS cgc,
+				cs_genericclassifiertype AS cgct
+				WHERE clas.classifierseq = cgc.classifierseq
+					AND clas.selectorid = cgct.genericclassifiertypeseq
+					AND cgct.NAME = 'Forma de Pago')
+			)
+	) AS t
+	WHERE 
+  	rn = 1;
+
 	CALL EXT.LIB_GLOBAL_CESCE:w_debug( i_Tenant, 'INSERTADOS ' || To_Varchar(::ROWCOUNT) || ' REGISTROS EN LA TABLA CS_STAGEGENERICCLASSIFIER', vProcedure, io_contador );
 	
 	CALL EXT.LIB_GLOBAL_CESCE:w_debug( i_Tenant, 'INSERT TABLA VIRTUAL ', vProcedure, io_contador );
